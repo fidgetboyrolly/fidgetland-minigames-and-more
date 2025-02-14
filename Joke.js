6e622b3ccd25e7b2e046faf3a8e5ab8e4bd440a3
fidgetboyrolly <fidgetboyrolly@icloud.com>
2025-02-13 20:04:26 -0800

    Create Joke.js

---
+++ Joke.js
@@ -0,0 +1,5 @@
+console.log("welcome to fidgetland minigamse and more!");
+console.log("why are you in the console anyway");
+console.log("you have no reason to go into the console of a gameless website");
+console.log("why is there stuff in the console in the first place you ask?")
+console.log("as a joke")
